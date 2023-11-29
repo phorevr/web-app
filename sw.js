9b97737a1fc9adb6e6992791705e134664b04887
Anton Pashkovskyi <anton@globalbit.co.il>
2023-11-29 17:38:24 +0100

    build

--- sw.js
+++ sw.js
@@ -1,28 +1,40 @@
+const CACHE_TOKEN_ENDPOINT = '/cache/token';
+
 self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
 self.addEventListener('install', (event) => {
-
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (!/\/auth\/(signup|restore)/.test(event.request.url)) {
-    return;
+  const { request, request: { url, method } } = event;
+  if (!url.match(CACHE_TOKEN_ENDPOINT)) {
+    return event;
   }
-  event.respondWith(
-    (async () => {
-      const reponse = await fetch(event.request.clone());
-      if (!event.clientId) return reponse;
-
-      const client = await self.clients.get(event.clientId);
-      if (!client) return reponse;
-
-      const token = (await reponse.clone().json()).token;
-
-      client.postMessage(token);
-
-      return reponse;
-    })(),
-  );
+  const dummyResponse = new Response('{}', { headers: { 'content-type': 'application/json' } });
+  if (method === 'POST') {
+    request.json().then(body => {
+      caches.open(CACHE_TOKEN_ENDPOINT).then(function (cache) {
+        cache.put(
+          CACHE_TOKEN_ENDPOINT,
+          new Response(JSON.stringify(body), {
+            headers: { 'content-type': 'application/json' }
+          }),
+        );
+      });
+    });
+    return dummyResponse;
+  } else {
+    event.respondWith(
+      caches.open(CACHE_TOKEN_ENDPOINT).then(function (cache) {
+        return cache.match(CACHE_TOKEN_ENDPOINT).then(function (response) {
+          return response || dummyResponse;
+        }) || dummyResponse;
+      })
+    );
+  }
 });
+
+importScripts('flutter_service_worker.js');
