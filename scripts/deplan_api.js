8d9986aeab5496f2f36dd0a29b02880bd35cbb72
Anton Pashkovskyi <anton@globalbit.co.il>
2024-04-22 18:11:50 +0200

    build

---
+++ scripts/deplan_api.js
@@ -0,0 +1,13 @@
+let client;
+
+async function signIn() {
+  client = client || new DePlanClient('Hgxbh8HpmWyQaznkFo1tj1xs5gwdHsNtbhU6amaWzbaw');
+  const res = await client.signIn();
+  return JSON.stringify(res);
+}
+
+async function signTransaction(transaction) {
+  client = client || new DePlanClient('Hgxbh8HpmWyQaznkFo1tj1xs5gwdHsNtbhU6amaWzbaw');
+  const res = await client.signTransaction({ transaction });
+  return JSON.stringify(res);
+}
